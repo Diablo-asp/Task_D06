3133ad5cb3993a8d87c30a191394f320ba19bc2e
Diablo-Asp <Di0ab0lo0@gmail.com>
2025-03-29 21:16:10 +0200

    2 opject

---
+++ Exception if/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Exception_String
+{
+    internal class Program
+    {
+        public static void cheak(string vioue, ref bool found)
+        {
+            string vowels1 = "aouie";
+            for (int i = 0; i < vioue.Length; i++)
+            {
+                if (vowels1.Contains(vioue[i]))
+                {
+                    found = true;
+                    throw new Exception($"{vioue} dosen't countains vowels");
+                }
+                else if (!found)
+                {
+                    found = false;
+                    Console.WriteLine($"{vioue} countains vowels");
+                    break;
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            string vowels = " ";
+            bool found = false;
+            do
+            {
+                Console.WriteLine("search for vowels");
+                vowels = Console.ReadLine().ToLower();
+                try
+                {
+                    cheak(vowels, ref found);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"{e.Message}");
+                }
+            } while (vowels != "X");
+        }
+    }
+}
---
+++ exception if task/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.Design;
+using System.Net.NetworkInformation;
+using System.Threading.Channels;
+
+namespace exception_if_task
+{
+    internal class Program
+    {
+        static void addNum(List<int> exceptions, int num)
+        {
+            if (!exceptions.Contains(num))
+            {
+                exceptions.Add(num);
+            }
+            else if (exceptions.Contains(num))
+            {
+                throw new Exception($"this {num} is aready in the list");
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<int> exceptions = new List<int>();
+            Console.WriteLine("if u need to Exit the loop = X ");
+            for (int i = 0; i != 'X'; i++)
+            {
+                try
+                {
+                    Console.WriteLine("Right Number to add ");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    addNum(exceptions, num);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error : {e.Message}");
+                }
+            }
+
+        }
+    }
+}
